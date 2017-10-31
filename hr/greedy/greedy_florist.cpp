89adf4f3b65028255cdbf236e1cbcc2cde3b3626
Nitish Puri <npuri1903@gmail.com>
2017-11-01 01:52:49 +0530

    more simple greedy problems.

---
+++ hr/greedy/greedy_florist.cpp
@@ -0,0 +1,44 @@
+// https://www.hackerrank.com/challenges/greedy-florist/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define PRINTVAR(a) cout << #a << " :: " << a << endl; 
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int n , k;
+    cin >> n >> k;
+    
+    vector<int> cost(n);
+    for(int i = 0; i < n; i++){
+        cin >> cost[i];
+    }
+    
+    //int f = ceil(float(n)/float(k));
+    
+    sort(cost.begin(), cost.end());
+    
+    long tc = 0;
+    int m = 1;
+    int f = 0;
+    for(int i = n-1; i >= 0; i-- ){
+        if(f == k){
+            m += 1;
+            f = 0;
+            //PRINTVAR(m);
+        }
+        //PRINTVAR(cost[i]*m);
+        tc += cost[i]*m;
+        f += 1;
+    }
+    
+    cout << tc;
+    
+    
+    return 0;
+}
---
+++ hr/greedy/max_perim_triangle.cpp
@@ -0,0 +1,59 @@
+// https://www.hackerrank.com/challenges/maximum-perimeter-triangle/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define PRINTIVEC(v) cout << #v << " :: "; for(auto i : v) {cout << i << " , ";} cout << endl;
+#define PRINTVAR(a) cout << #a << " :: " << a << endl; 
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin >> n;
+    
+    vector<int> e(n);
+    
+    for(int i = 0; i < n; i++){
+        cin >> e[i];
+    }
+    
+    sort(e.begin(), e.end());
+    
+    //PRINTIVEC(e);
+    
+    int p = 0;
+    int a, b, c;
+    bool succcess = false;
+    
+    for(int i = n-1; i > 1; i--){
+        a = e[i];
+        b = e[i-1];
+        c = e[i-2];
+        //PRINTVAR(a);
+        //PRINTVAR(b);
+        //PRINTVAR(c);
+        
+        
+        if( (b+c) <= a){
+            continue;       // degenerate
+        }
+        else{
+            p = (a+b+c);
+            succcess = true;
+            break;
+        }        
+    }
+    
+    //PRINTVAR(p);
+    if(succcess)
+        cout << c << ' ' << b << ' ' << a;
+    else
+        cout << -1;
+    
+    
+    return 0;
+}
