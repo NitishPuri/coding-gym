3680a1e0a07355f32019020e5481365ea38a4665
Nitish Puri <npuri1903@gmail.com>
2019-08-06 03:46:02 +0530

    configuring vscode with vs 2017

--- cmake.bat
+++
@@ -1,9 +0,0 @@
-:: Generate visual studio solution files from the cmake lists.
-@echo off
-
-cd build
-
-If exist CMakeCache.txt del CMakeCache.txt
-
-cmake -G "Visual Studio 14 2015 Win64" ..
-cd ..
--- visualStudio.bat
+++
@@ -1,6 +0,0 @@
-:: Open the project in visual studio.
-@echo off
-
-call env.bat
-
-devenv build\practice-cpp.sln
