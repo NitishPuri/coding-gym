0359098f7a80d8ee92720c02b4dbcaa52a480716
Nitish Puri <npuri1903@gmail.com>
2019-08-07 20:55:37 +0530

    template : making_candies

---
+++ hr/search/making_candies.cpp
@@ -0,0 +1,70 @@
+// https://www.hackerrank.com/challenges/making-candies/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumPasses function below.
+long minimumPasses(long m, long w, long p, long n)
+{
+}
+
+int main()
+{
+    // ofstream fout(getenv("OUTPUT_PATH"));
+
+    string mwpn_temp;
+    getline(cin, mwpn_temp);
+
+    vector<string> mwpn = split_string(mwpn_temp);
+
+    long m = stol(mwpn[0]);
+
+    long w = stol(mwpn[1]);
+
+    long p = stol(mwpn[2]);
+
+    long n = stol(mwpn[3]);
+
+    long result = minimumPasses(m, w, p, n);
+
+    cout << result << "\n";
+
+    // fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string)
+{
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
+        return x == y && x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ')
+    {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos)
+    {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
