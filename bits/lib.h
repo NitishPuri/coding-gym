180bf26665a767f09052c190af3dec0098305299
Puri, Nitish <nitish.puri@stryker.com>
2019-08-13 00:12:33 +0530

    Add to lib : pearson coefficient, least sq regression.

--- bits/lib.h
+++ bits/lib.h
@@ -102,6 +102,24 @@ vector<size_t> rankify(const vector<T>& x) {
     return rank;
 }
 
+// Pearson coefficient.
+double pearson(const vector<double>& x, const vector<double>& y) {
+    auto mx = mean(x), my = mean(y);
+    auto n = x.size();
+
+    double cov = 0.0, sd_x = 0.0, sd_y = 0.0;
+    for(int i = 0; i < n; ++i) {
+        cov += (x[i] - mx) * (y[i] - my);
+        sd_x += pow(x[i] - mx, 2);
+        sd_y += pow(y[i] - my, 2);
+    }
+
+    cov /= n;
+    sd_x = sqrt(sd_x / n);
+    sd_y = sqrt(sd_y / n);
+
+    return cov / (sd_x * sd_y);
+}
 
 // Spearman coefficient in case of unique values in datasets.
 double spearman(const vector<double>& x, const vector<double>& y) {
@@ -117,6 +135,39 @@ double spearman(const vector<double>& x, const vector<double>& y) {
 
     auto res = 1. - (6 * sum_d) / (n * (n*n - 1));
     return res;
+}
+
+// Perform least squares linear regression on samples and return a function represnting the line.
+function<double (double)> lease_sq_reg(const vector< pair < double , double > >& samples, double& rms) {
+    double n = samples.size();
+
+    double sum_x = 0.0;
+    double sum_y = 0.0;
+    double sum_xx = 0.0;
+    double sum_xy = 0.0;
+    for(auto& sample : samples) {
+        sum_x += sample.first;
+        sum_y += sample.second;
+        sum_xx += pow(sample.first, 2);
+        sum_xy += (sample.first * sample.second);
+    }
+
+    auto m_x = sum_x / n;
+    auto m_y = sum_y / n;
+
+    double b = ((n * sum_xy) - (sum_x*sum_y)) / ((n * sum_xx) - pow(sum_x, 2));
+    double a = m_y - b * m_x;
+
+    auto reg = [a, b](double x) { return a + b*x; };
+
+    double sse = 0.0;
+    for(auto& sample : samples) {
+        sse += pow(reg(sample.first) - sample.second, 2);
+    }
+
+    rms = sqrt(sse / n);
+
+    return reg;
 }
 
 
