7b571d49d0e36bc5b47acf5efe5e6c6db397a72e
Nitish Puri <npuri1903@gmail.com>
2019-08-31 09:02:40 +0530

    formatting.

--- hr/search/making_candies.cpp
+++ hr/search/making_candies.cpp
@@ -8,127 +8,112 @@ vector<string> split_string(string);
 
 // Complete the minimumPasses function below.
 using ull = unsigned long long;
-ull minimumPasses(ull m, ull w, ull p, ull n)
-{
-    ull curr = 0;
-    ull days = 0;
+ull minimumPasses(ull m, ull w, ull p, ull n) {
+  ull curr = 0;
+  ull days = 0;
 
-    // long min_days = 0;
-    // long max_days = ceill(n / (m * w));
+  // long min_days = 0;
+  // long max_days = ceill(n / (m * w));
 
-    // while (min_days != max_days)
-    // {
-    //     long mid = (min_days + max_days) / 2;
-    //     // Calculate how much can be produced in 'mid' days
-    //     long min_production = m * w * mid;
-    // }
+  // while (min_days != max_days)
+  // {
+  //     long mid = (min_days + max_days) / 2;
+  //     // Calculate how much can be produced in 'mid' days
+  //     long min_production = m * w * mid;
+  // }
 
-    while (curr < n)
-    {
-        if (curr >= p)
-        {
-            ull rem = n - curr;
-            if (rem > m * w)
-            {
-                ull mw = curr / p;
+  while (curr < n) {
+    if (curr >= p) {
+      ull rem = n - curr;
+      if (rem > m * w) {
+        ull mw = curr / p;
 
-                if (m >= w + mw)
-                {
-                    w += mw;
-                }
-                else if (w >= m + mw)
-                {
-                    m += mw;
-                }
-                else
-                {
-                    ull total = m + w + mw;
-                    m = total / 2;
-                    w = total - m;
-                }
-
-                curr = (curr % p);
-            }
+        if (m >= w + mw) {
+          w += mw;
+        } else if (w >= m + mw) {
+          m += mw;
+        } else {
+          ull total = m + w + mw;
+          m = total / 2;
+          w = total - m;
         }
 
-        // Skip ahead to buy more resources.
-        ull skip = (p > curr) ? ceill(long double(p - curr) / (m * w)) : 1;
-
-        if (skip == 0)
-        {
-            skip = 1;
-        }
-
-        if (curr + (skip * m * w) > n)
-        {
-            ull rem = n - curr;
-            if (rem < m * w)
-            {
-                skip = 1;
-            }
-        }
-        curr += skip * m * w;
-
-        days += skip;
+        curr = (curr % p);
+      }
     }
 
-    return days;
+    // Skip ahead to buy more resources.
+    ull skip = (p > curr) ? ceill(long double(p - curr) / (m * w)) : 1;
+
+    if (skip == 0) {
+      skip = 1;
+    }
+
+    if (curr + (skip * m * w) > n) {
+      ull rem = n - curr;
+      if (rem < m * w) {
+        skip = 1;
+      }
+    }
+    curr += skip * m * w;
+
+    days += skip;
+  }
+
+  return days;
 }
 
-int main()
-{
-    // ofstream fout(getenv("OUTPUT_PATH"));
+int main() {
+  // ofstream fout(getenv("OUTPUT_PATH"));
 
-    string mwpn_temp;
-    getline(cin, mwpn_temp);
+  string mwpn_temp;
+  getline(cin, mwpn_temp);
 
-    vector<string> mwpn = split_string(mwpn_temp);
+  vector<string> mwpn = split_string(mwpn_temp);
 
-    long m = stol(mwpn[0]);
+  long m = stol(mwpn[0]);
 
-    long w = stol(mwpn[1]);
+  long w = stol(mwpn[1]);
 
-    unsigned long long p = stoull(mwpn[2]);
+  unsigned long long p = stoull(mwpn[2]);
 
-    unsigned long long n = stoull(mwpn[3]);
+  unsigned long long n = stoull(mwpn[3]);
 
-    ull result = minimumPasses(m, w, p, n);
+  ull result = minimumPasses(m, w, p, n);
 
-    cout << result << "\n";
+  cout << result << "\n";
 
-    // fout.close();
+  // fout.close();
 
-    return 0;
+  return 0;
 }
 
-vector<string> split_string(string input_string)
-{
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
-        return x == y && x == ' ';
-    });
+vector<string> split_string(string input_string) {
+  string::iterator new_end =
+      unique(input_string.begin(), input_string.end(),
+             [](const char &x, const char &y) { return x == y && x == ' '; });
 
-    input_string.erase(new_end, input_string.end());
+  input_string.erase(new_end, input_string.end());
 
-    while (input_string[input_string.length() - 1] == ' ')
-    {
-        input_string.pop_back();
-    }
+  while (input_string[input_string.length() - 1] == ' ') {
+    input_string.pop_back();
+  }
 
-    vector<string> splits;
-    char delimiter = ' ';
+  vector<string> splits;
+  char delimiter = ' ';
 
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
+  size_t i = 0;
+  size_t pos = input_string.find(delimiter);
 
-    while (pos != string::npos)
-    {
-        splits.push_back(input_string.substr(i, pos - i));
+  while (pos != string::npos) {
+    splits.push_back(input_string.substr(i, pos - i));
 
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
+    i = pos + 1;
+    pos = input_string.find(delimiter, i);
+  }
 
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+  splits.push_back(
+      input_string.substr(i, min(pos, input_string.length()) - i + 1));
 
-    return splits;
+  return splits;
 }
--- hr/search/maximum_subarray_sum.cpp
+++ hr/search/maximum_subarray_sum.cpp
@@ -5,108 +5,99 @@ using namespace std;
 vector<string> split_string(string);
 
 // Complete the maximumSum function below.
-long maximumSum(vector<long> a, long m)
-{
-    long maxSum = 0;
+long maximumSum(vector<long> a, long m) {
+  long maxSum = 0;
 
-    // partial_sum(a.begin(), a.end(), a.begin(), [m](long long a, long long b) {
-    //     return (a % m + b % m) % m;
-    // });
+  // partial_sum(a.begin(), a.end(), a.begin(), [m](long long a, long long b) {
+  //     return (a % m + b % m) % m;
+  // });
 
-    long long curr = 0;
-    for (long &a_i : a)
-    {
-        long long a_m = a_i % m;
-        curr = (a_m + curr) % m;
-        a_i = curr;
+  long long curr = 0;
+  for (long &a_i : a) {
+    long long a_m = a_i % m;
+    curr = (a_m + curr) % m;
+    a_i = curr;
+  }
+
+  set<long> s;
+  for (auto itr_i = a.begin(); itr_i < a.end(); ++itr_i) {
+    auto upper = s.upper_bound(*itr_i);
+    maxSum = max(maxSum, *itr_i);
+    if (upper != s.end()) {
+      maxSum = max(maxSum, (*itr_i - *upper + m) % m);
     }
+    s.insert(*itr_i);
+  }
 
-    set<long> s;
-    for (auto itr_i = a.begin(); itr_i < a.end(); ++itr_i)
-    {
-        auto upper = s.upper_bound(*itr_i);
-        maxSum = max(maxSum, *itr_i);
-        if (upper != s.end())
-        {
-            maxSum = max(maxSum, (*itr_i - *upper + m) % m);
-        }
-        s.insert(*itr_i);
-    }
-
-    return maxSum;
+  return maxSum;
 }
 
-int main()
-{
-    // ofstream fout(getenv("OUTPUT_PATH"));
+int main() {
+  // ofstream fout(getenv("OUTPUT_PATH"));
 
-    int q;
-    cin >> q;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  int q;
+  cin >> q;
+  cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-    for (int q_itr = 0; q_itr < q; q_itr++)
-    {
-        string nm_temp;
-        getline(cin, nm_temp);
+  for (int q_itr = 0; q_itr < q; q_itr++) {
+    string nm_temp;
+    getline(cin, nm_temp);
 
-        vector<string> nm = split_string(nm_temp);
+    vector<string> nm = split_string(nm_temp);
 
-        int n = stoi(nm[0]);
+    int n = stoi(nm[0]);
 
-        long m = stol(nm[1]);
+    long m = stol(nm[1]);
 
-        string a_temp_temp;
-        getline(cin, a_temp_temp);
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
 
-        vector<string> a_temp = split_string(a_temp_temp);
+    vector<string> a_temp = split_string(a_temp_temp);
 
-        vector<long> a(n);
+    vector<long> a(n);
 
-        for (int i = 0; i < n; i++)
-        {
-            long a_item = stol(a_temp[i]);
+    for (int i = 0; i < n; i++) {
+      long a_item = stol(a_temp[i]);
 
-            a[i] = a_item;
-        }
-
-        long result = maximumSum(a, m);
-
-        cout << result << "\n";
+      a[i] = a_item;
     }
 
-    // fout.close();
+    long result = maximumSum(a, m);
 
-    return 0;
+    cout << result << "\n";
+  }
+
+  // fout.close();
+
+  return 0;
 }
 
-vector<string> split_string(string input_string)
-{
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [](const char &x, const char &y) {
-        return x == y && x == ' ';
-    });
+vector<string> split_string(string input_string) {
+  string::iterator new_end =
+      unique(input_string.begin(), input_string.end(),
+             [](const char &x, const char &y) { return x == y && x == ' '; });
 
-    input_string.erase(new_end, input_string.end());
+  input_string.erase(new_end, input_string.end());
 
-    while (input_string[input_string.length() - 1] == ' ')
-    {
-        input_string.pop_back();
-    }
+  while (input_string[input_string.length() - 1] == ' ') {
+    input_string.pop_back();
+  }
 
-    vector<string> splits;
-    char delimiter = ' ';
+  vector<string> splits;
+  char delimiter = ' ';
 
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
+  size_t i = 0;
+  size_t pos = input_string.find(delimiter);
 
-    while (pos != string::npos)
-    {
-        splits.push_back(input_string.substr(i, pos - i));
+  while (pos != string::npos) {
+    splits.push_back(input_string.substr(i, pos - i));
 
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
+    i = pos + 1;
+    pos = input_string.find(delimiter, i);
+  }
 
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+  splits.push_back(
+      input_string.substr(i, min(pos, input_string.length()) - i + 1));
 
-    return splits;
+  return splits;
 }
