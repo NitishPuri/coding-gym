d2212b5395b4442012ce7b5129130c80eb2af671
Nitish Puri <npuri1903@gmail.com>
2022-10-04 01:00:14 +0530

    more solutions.

---
+++ leetcode/array/maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());        
+        int pre = 0;
+        for(auto a : arr) {
+            pre = min(pre + 1, a);
+        }        
+        return pre;        
+    }
+};
